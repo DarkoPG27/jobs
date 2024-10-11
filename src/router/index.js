af23ff0be42953a0818e6e0ea1143193b400f611
DarkoPg27 <darkobrnovic27@gmail.com>
2024-10-11 17:15:27 +0200

    created not found view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotfoundView from "@/views/NotfoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+
+    {
+      path: "/:catcAll(.*)",
+      name: "not-found",
+      component: NotfoundView,
     },
   ],
 });
